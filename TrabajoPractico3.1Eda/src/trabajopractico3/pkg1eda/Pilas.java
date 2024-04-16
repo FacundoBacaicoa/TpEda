57b708af8f5f50d9a3fbf70d41d16ee02efa132b
FacundoBiel <facubiel19@gmail.com>
2024-04-16 18:06:07 -0300

    TP EDA 3.2

--- TrabajoPractico3.1Eda/src/trabajopractico3/pkg1eda/Pilas.java
+++ TrabajoPractico3.1Eda/src/trabajopractico3/pkg1eda/Pilas.java
@@ -1,23 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package trabajopractico3.pkg1eda;
 
 import java.util.Scanner;
 import java.util.Stack;
 
-/**
- *
- * @author facun
- */
 public class Pilas {
 
     Stack<Integer> pila = new Stack<Integer>();
 
     Stack<Integer> pila1 = new Stack<Integer>();
+    
+    Stack<Integer> pila2= new Stack<Integer>();
 
-    public Pilas() {
+   public Pilas() {
     }
 
     public Stack<Integer> getPila1() {
@@ -36,6 +30,16 @@ public class Pilas {
         this.pila = pila;
     }
 
+    public Stack<Integer> getPila2() {
+        return pila2;
+    }
+
+    public void setPila2(Stack<Integer> pila2) {
+        this.pila2 = pila2;
+    }
+    
+    
+
     public void metodoPush() {
         //Pila 1
         
@@ -49,8 +53,7 @@ public class Pilas {
              System.out.println(pila);
              break;
          }
-            
-        
+       System.out.println("-----------------------");
         //Pila 2
         pila1.push(13);
         pila1.push(12);
@@ -63,5 +66,30 @@ public class Pilas {
          }
             
     }
-
+    
+    public void pilaResult(){
+                while (!pila.isEmpty() && !pila1.isEmpty()) {
+            int elemento = pila.pop();
+            pila2.push(elemento);
+            elemento=pila1.pop();
+            pila2.push(elemento);
+        }
+    }
+    
+    public void ordenarPila(){
+         int i, j, temp = 0;
+         int tamaño=pila2.size();
+ for (i = 0; i < tamaño; i++) {
+ for (j = 0; j < tamaño-1; j++) {
+ if (pila2.get(j) < pila2.get(j+1)) {
+     temp = pila2.get(j);
+      pila2.set(j, pila2.get(j + 1));
+      pila2.set(j + 1, temp);
+ }
+      } 
+    }
+        System.out.println("Pila 3");
+        System.out.println(pila2);
+  }
 }
+
